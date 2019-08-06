ca3f1e20a244c4e16cd7ed8aa1078987617dc05c
Foka tatsinkou Robert bright <fokrober@e1r8p1.1337.ma>
2019-08-06 14:51:38 +0100

    using ft_memcpy and add sort method

--- sort_list/list.h
+++ sort_list/list.h
@@ -6,13 +6,15 @@
 /*   By: zaz <zaz@staff.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/27 13:14:10 by zaz               #+#    #+#             */
-/*   Updated: 2019/07/29 03:26:42 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/08/06 14:31:55 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIST_H
 # define LIST_H
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 typedef struct s_list t_list;
 
 struct s_list
--- sort_list/sort_list.c
+++ sort_list/sort_list.c
@@ -6,17 +6,44 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 02:59:31 by fokrober          #+#    #+#             */
-/*   Updated: 2019/08/05 14:25:22 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/08/06 14:49:35 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
 
-void	ft_swap(int *a, int *b)
+void	printab(t_list *head)
 {
-	*a = *a + *b;
-	*b = *a - *b;
-	*a = *a - *b;
+	while (head)
+	{
+		printf("[%d]\t", head->data);
+		head = head->next;
+	}
+	printf("\n");
+}
+
+void	*ft_memcpy(void *dst, void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
+
+void	ft_swap(t_list *a, t_list *b)
+{
+	t_list	tmp;
+
+	(void)ft_memcpy(&tmp, b, sizeof(t_list));
+	(void)ft_memcpy(b, a, sizeof(t_list));
+	(void)ft_memcpy(a, &tmp, sizeof(t_list));
+	a->next = b->next;
+	b->next = tmp.next;
 }
 
 int		croissant(int a, int b)
@@ -24,55 +51,48 @@ int		croissant(int a, int b)
 	return (a <= b);
 }
 
-t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+t_list	*create_list(int n)
 {
-	(void)lst;
-	printf("%d\n", cmp(2, 1));
-	return (0);
-}
+	t_list	*head;
 
-void	printab(int *tab, int n)
-{
-	int		i;
-
-	i = 0;
-	while (i < n)
-		printf("%d\t", tab[i++]);
-	printf("\n");
-}
-
-int		sort(int *a, int len)
-{
-	int			i;
-	int			j;
-	int			order;
-
-	i = 0;
-	order = 1;
-	while (i < len - 1)
+	head = (t_list*)malloc(sizeof(t_list));
+	head->data = 0;
+	head->next = NULL;
+	if (n)
 	{
-		j = i + 1;
-		order = 1;
-		while (j < len && (order = croissant(a[i], a[j])))
-			j++;
-		if (!order)
-			ft_swap(a + i, a + j);
-		i++;
+		head->data = arc4random_uniform(100);
+		head->next = create_list(n - 1);
 	}
-	if (len && !order)
-		return (sort(a, len - 1));
-	return (1);
+	return (head);
+}
+
+t_list	*sort_list(t_list *head, int (*cmp)(int, int))
+{	
+	t_list		*nhead;
+	t_list		*tmp;
+
+	tmp = head;
+	while (head && head->next)
+	{
+		nhead = head->next;
+		while (nhead->next)
+		{
+			if (!cmp(head->data, nhead->data))
+				ft_swap(head, nhead);
+			nhead = nhead->next;
+		}
+		head = head->next;
+	}
+	return (tmp);
 }
 
 int		main(void)
 {
-	int		a[] = {1, 6, 9, 2, 1, 4, 11, 5, 7, 0, -1};
-	int		len;
-	t_list	t;
+	t_list	*t;
 
-	len = sizeof(a) / 4;
-	sort(a, len);
-	printab(a, len);
-	(void)sort_list(&t , &croissant);
+	t = create_list(10);
+	printab(t);
+	(void)sort_list(t , &croissant);
+	printab(t);
 	return (0);
 }
