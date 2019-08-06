76f3ba1e2fa101577a63c858788540134e53b96b
Foka tatsinkou Robert bright <fokrober@e1r8p1.1337.ma>
2019-08-06 14:56:20 +0100

    subject

--- sort_list/list.h
+++ sort_list/list.h
@@ -6,15 +6,10 @@
 /*   By: zaz <zaz@staff.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/27 13:14:10 by zaz               #+#    #+#             */
-/*   Updated: 2019/08/06 14:31:55 by fokrober         ###   ########.fr       */
+/*   Updated: 2014/02/27 13:15:29 by zaz              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIST_H
-# define LIST_H
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
 typedef struct s_list t_list;
 
 struct s_list
@@ -22,4 +17,3 @@ struct s_list
 	int     data;
 	t_list  *next;
 };
-#endif
